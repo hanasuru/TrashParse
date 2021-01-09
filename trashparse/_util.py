0cf32eff066d33c92b7d9536e716b4d89dbd7833
hanasuru <faqih.insani@ymail.com>
2021-01-09 20:31:37 +0700

    hotfix-0.1.1: Resolve bad unicode & file duplication handling

--- trashparse/_util.py
+++ trashparse/_util.py
@@ -1,10 +1,16 @@
 from __future__ import division
 from datetime import datetime, timedelta, tzinfo
 from calendar import timegm
+from pathlib import Path
 
 EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as MS file time
 HUNDREDS_OF_NANOSECONDS = 10000000
 HOUR = timedelta(hours=1)
+
+UNITS = {
+    1000: ['KB', 'MB', 'GB'],
+    1024: ['KiB', 'MiB', 'GiB']
+}
 
 
 class UTC(tzinfo):
@@ -16,6 +22,9 @@ class UTC(tzinfo):
 
     def dst(self, dt):
         return timedelta(0)
+
+
+utc = UTC()
 
 
 class UTCTime(object):
@@ -32,3 +41,20 @@ class UTCTime(object):
             dt = dt.replace(tzinfo=utc)
     
         return EPOCH_AS_FILETIME + (timegm(dt.timetuple()) * HUNDREDS_OF_NANOSECONDS)
+
+
+def approximate_date(timestamp):
+    return str(UTCTime.time_to_date(timestamp))
+
+def approximate_time(timestamp):
+    dt = approximate_date(timestamp)
+    td = dt - datetime(1970, 1, 1)
+
+    return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6 
+
+def approximate_size(size, flag_1024_or_1000=True):
+    mult = 1000
+    for unit in UNITS[mult]:
+        size = size / mult
+        if size < mult:
+            return '{0:.3f} {1}'.format(size, unit)
