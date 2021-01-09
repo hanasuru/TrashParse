ad38ac3d9adb64b4acfb67923fe3920ebadfcc78
hanasuru <faqih.insani@ymail.com>
2021-01-09 20:28:43 +0700

    hotfix-0.1.1: Resolve improper setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@ from distutils.core import setup
 setup(
   name = 'trashparse',
   packages = ['trashparse'],
-  version = '0.1',
+  version = '0.1.1',
   license='MIT',
   description = 'simple tool for analyzing Windows Recycle.Bin',
   author = 'hanasuru',
   author_email = 'faqih.insani@ymail.com',
-  url = 'https://github.com/hanasuru/TrashParse',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/hanasuru/TrashParse/archive/0.1.0.tar.gz',    # I explain this later on
-  keywords = ['forensic', 'recycle-bin', 'windows'],   # Keywords that define your package best
+  url = 'https://github.com/hanasuru/TrashParse',
+  download_url = 'https://github.com/hanasuru/TrashParse/archive/0.1.1.tar.gz',
+  keywords = ['forensic', 'recycle-bin', 'windows'],
   install_requires=['prettytable'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 2.7',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
   ],
   scripts=['bin/trashparse'],
 )
